399579ee44eefff40d9509a542173a2407dee3b7
Kevin Gida <139972073+KevinGida@users.noreply.github.com>
2023-11-08 10:44:11 +0100

    add backend

---
+++ figureforge-backend/src/main/java/com/salt/figureforgebackend/FigureforgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.salt.figureforgebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigureforgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FigureforgeBackendApplication.class, args);
+	}
+
+}
