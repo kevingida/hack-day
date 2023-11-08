6ba7eaebdeef2f699adf3af2ef20137ed5c0f839
Kevin Gida <139972073+KevinGida@users.noreply.github.com>
2023-11-08 10:16:43 +0100

    add backend

---
+++ toytroop-backend/src/main/java/com/salt/toytroopbackend/ToytroopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.salt.toytroopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToytroopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToytroopBackendApplication.class, args);
+	}
+
+}
---
+++ toytroop-backend/src/test/java/com/salt/toytroopbackend/ToytroopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salt.toytroopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToytroopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
