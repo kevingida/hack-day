d805f7687197120029de37f8336519767eb167ac
Kevin Gida <139972073+KevinGida@users.noreply.github.com>
2023-11-09 11:46:19 +0100

    manage to get card

--- figureforge-backend/src/main/java/com/salt/figureforgebackend/repositories/PictureRepository.java
+++ figureforge-backend/src/main/java/com/salt/figureforgebackend/repositories/PictureRepository.java
@@ -1,12 +1,13 @@
 package com.salt.figureforgebackend.repositories;
 
 import com.salt.figureforgebackend.models.Picture;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PictureRepositories extends ListCrudRepository<Picture, Long> {
+public interface PictureRepositories extends JpaRepository<Picture, Long> {
     List<Picture> findByToyId(Long id);
 }
